a1f9c0072fc2b3e37c08bd63052abcc21cdebca5
shellby.miller <shellby.miller@gmail.com>
2022-11-06 14:08:15 -0500

    added password length criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Password criteria variables
+//var passwordLength;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +15,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Function
+function generatePassword(){
+
+//Input password length
+  let passwordLength = prompt("Please choose a password length between 8 and 128 characters.");
+  console.log(passwordLength);
+
+  if(passwordLength = "false"){
+    alert("Input required.");
+
+  } else if (passwordLength <8 || passwordLength >128){
+    passwordLength = prompt("Value must be between 8-128 characters.");
+    console.log(passwordLength);
+  } 
+  }
