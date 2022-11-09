abc705a9ab372b961bf059f74649424a38627d00
shellby.miller <shellby.miller@gmail.com>
2022-11-08 19:12:47 -0500

    added criteria function for lowercase

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 // Password criteria variables
-//var passwordLength;
+var passwordLength;
+var lowercase;
+var uppercase;
+var numeric;
+var specialCharacter;
 
 // Write password to the #password input
 function writePassword() {
@@ -20,12 +24,18 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword(){
 
 //Input password length
-  let passwordLength = prompt("Please use a numeric value to choose a password length between 8 and 128 characters.");
+  passwordLength = prompt("Please use a numeric value to choose a password length between 8 and 128 characters.");
   console.log(`Password Length: ${passwordLength}`);
 
   if(!passwordLength || passwordLength <8 || passwordLength >128 || isNaN(passwordLength)){
     alert("Please input a numeric value between 8 and 128.");
     console.log(`Password Length: ${passwordLength}`);
+  }
+;
+  //Confirm lowercase
+  lowercase = prompt("Will the password contain a lowercase letter?");
+  console.log(`Contains Lowercase:` + lowercase);
 
-  }
-  }
+}
+  //lowercase, uppercase, numeric, and/or special characters
+
