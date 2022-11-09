bb6ee14c1bc6ee0878819fa63e9c28d5ad0e70f7
shellby.miller <shellby.miller@gmail.com>
2022-11-08 20:32:27 -0500

    updated password length code to reopen prompt if false input

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,7 @@ var uppercase;
 var numeric;
 var specialCharacter;
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -24,13 +25,19 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword(){
 
 //Input password length
-  passwordLength = prompt("Please use a numeric value to choose a password length between 8 and 128 characters.");
-  console.log(`Password Length: ${passwordLength}`);
+passwordLength = prompt("Please use a numeric value to choose a password length between 8 and 128 characters.");
 
+//Alert if input is false, not numeric, or not between 8-128 and reopen prompt for user input
   if(!passwordLength || passwordLength <8 || passwordLength >128 || isNaN(passwordLength)){
-    alert("Please input a numeric value between 8 and 128.");
-    console.log(`Password Length: ${passwordLength}`);
-  }
+    alert("Please input a numeric value between 8 and 128."); 
+    generatePassword();
+
+  } 
+
+else { 
+
+  //Only log password length in console if value is true and numeric input between 8-128
+  console.log(`Password Length: ${passwordLength}`);
 
   //Confirm lowercase
   lowercase = confirm("Will the password contain a lowercase letter?");
@@ -47,7 +54,9 @@ function generatePassword(){
   //Confirm special character
   specialCharacter = confirm("Will the password contain a special character?");
   console.log(`Contains Special Character:${specialCharacter}`);
-  
+
+  }
+
 };
-  //lowercase, uppercase, numeric, and/or special characters
+
 
