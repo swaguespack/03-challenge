ecb38f85196cd16924fd4cf5c273d4afbc97d39a
shellby.miller <shellby.miller@gmail.com>
2022-11-10 22:05:40 -0500

    refactored code to ensure one character from selected criteria included in password

--- Develop/script.js
+++ Develop/script.js
@@ -8,23 +8,17 @@ var userUppercase;
 var userNumeric;
 var userSpecialCharacter;
 
-//Array variables to place user input
-var userInput;
-var trueInput;
+//Variables to place characters for user input
+var userInput=[];
+var includeChar=[];
 
-//Array variables to test generator output
-var containsLower;
-var containsUpper;
-var containsNum;
-var containsSpecial;
-
-//Define possible input for password criteria
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercase = uppercase.map(element => {
-  return element.toLowerCase();
-});
-var number = [0,1,2,3,4,5,6,7,8,9];
-var specialCharacter = ["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[", "\\" ,"]","^","_","`","{","|","}","~"];
+//Object storing password character options
+var passwordOptions ={
+uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+lowercase: "abcdefghijklmnopqrstuvwxyz",
+number: "0123456789",
+specialCharacter: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~,"
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -33,7 +27,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -48,87 +42,61 @@ userPasswordLength = prompt("Please use a numeric value to choose a password len
   if(!userPasswordLength || userPasswordLength <8 || userPasswordLength >128 || isNaN(userPasswordLength)){
     alert("Please input a numeric value between 8 and 128."); 
     generatePassword();
-  } 
-else { 
+  }
+ else {
   //Only log password length in console if value is true and numeric input between 8-128
   console.log(`Password Length: ${userPasswordLength}`);
+ 
+  //Confirm and collect user input
+  //Contains lowercase?
+  userLowercase = confirm("Will the password contain a lowercase letter?");  
+  if(userLowercase){
+    console.log("Lowercase: ", userLowercase);
+    userInput += passwordOptions.lowercase;
+    includeChar.push(passwordOptions.lowercase[Math.floor(Math.random()*passwordOptions.lowercase.length)]);
+  };
+ //Contains uppercase?
+ userUppercase = confirm("Will the password contain an uppercase letter?");
+ if(userUppercase){
+  console.log("Uppercase: ",userUppercase);
+  userInput += passwordOptions.uppercase;
+  includeChar.push(passwordOptions.uppercase[Math.floor(Math.random()*passwordOptions.uppercase.length)]);
+ };
+ //Contains number?
+ userNumeric = confirm("Will the password contain a number?");
+ if(userNumeric){
+  console.log("Number: ",userNumeric);
+  userInput += passwordOptions.number;
+  includeChar.push(passwordOptions.number[Math.floor(Math.random()*passwordOptions.number.length)]);
+ };
+  //Contains special character?
+  userSpecialCharacter = confirm("Will the password contain a special character?");
+  if(userSpecialCharacter){
+   console.log("Special Character: ",userSpecialCharacter);
+   userInput += passwordOptions.specialCharacter;
+   includeChar.push(passwordOptions.specialCharacter[Math.floor(Math.random()*passwordOptions.specialCharacter.length)]);
+  };
 
-  //Confirm desired criteria
-  if (confirm("Will the password contain a lowercase letter?") == true) {
-    userLowercase = lowercase;
-    console.log(`Contains lowercase: ${userLowercase}`);
-  }  if (confirm("Will the password contain an uppercase letter?") == true) {
-    userUppercase = uppercase;
-    console.log(`Contains Uppercase: ${userUppercase}`);
-  } if (confirm("Will the password contain a number?") == true) {
-    userNumeric = number;
-    console.log(`Contains Number: ${userNumeric}`);
-  } if (confirm("Will the password contain a special character?") == true) {
-    userSpecialCharacter = specialCharacter;
-    console.log(`Contains Special Character: ${userSpecialCharacter}`);
-  }
-};
-
-//Result if no character criteria selected: alert and return user to first prompt
-if (!userLowercase && !userUppercase && !userNumeric && !userSpecialCharacter){
+  //Result if no character criteria selected: alert and return user to first prompt
+  if (!userLowercase && !userUppercase && !userNumeric && !userSpecialCharacter){
   alert("Please choose a criteria.");
   generatePassword();
  };
 
-//Result if one or more character criteria selected: concat input to array and remove undefined
-userInput = userLowercase.concat(userUppercase,userNumeric,userSpecialCharacter);
-trueInput = userInput.filter(function(element){
-  return element !== undefined;
-});
-console.log(trueInput);
-
-//Empty variable for password
- var passwordEmpty = [];
-
- //For loop for random selection
- for (var i=0; i<userPasswordLength;i++){
-  var result = trueInput[Math.floor(Math.random() * trueInput.length)];
-  passwordEmpty.push(result); 
-  console.log(result);
+ //Loop to ensure password contains correct amount of characters
+ while(includeChar.length < userPasswordLength){
+  includeChar.push(userInput[Math.floor(Math.random()*userPasswordLength)]);
  };
 
- //Check if generated password contains at least one character from each of the selected criteria
- //Contains lowercase?
- containsLower = passwordEmpty.some(element =>{
-  return userLowercase.includes(element);
-});
-console.log(containsLower);
-//Contains uppercase?
-containsUpper = passwordEmpty.some(element =>{
-  return userUppercase.includes(element);
-});
-console.log(containsUpper);
-//Contains numeric?
-containsNum = passwordEmpty.some(element =>{
-  return userNumeric.includes(element);
-});
-console.log(containsNum);
-//Contains special character?
-containsSpecial = passwordEmpty.some(element =>{
-  return userSpecialCharacter.includes(element);
-});
-console.log(containsSpecial);
+ //Join password - shuffle the string - return the shuffled password
+var password = includeChar.join("");
+password = password.toString();
 
-//If the password is missing a selection, alert and try again
-if (!containsLower || !containsUpper || !containsNum || !containsSpecial) {
-  alert("Generator failed. Please try again.");
-  generatePassword();
-};
-
- //Join password and return
-var password = passwordEmpty.join("");
 console.log("Your Password Is: " + password);
 return password;
-  
+
 };
-
-
-
+};
 
 
 
